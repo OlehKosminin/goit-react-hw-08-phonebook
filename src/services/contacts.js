369dd2ccedd2c4ab6d1af9a506042b58e4785bda
Olehys <arzamazova13@gmail.com>
2023-03-12 12:28:09 +0200

    add react router dom

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,22 +1,17 @@
-import axios from 'axios';
-
-const contactInstance = axios.create({
-  baseURL: 'https://64048bc680d9c5c7baca34b4.mockapi.io/contacts',
-});
+import instance from './auth';
 
 export const getAllContacts = async () => {
-  const { data } = await contactInstance.get('/');
-  // console.log(data);
+  const { data } = await instance.get('/contacts');
   return data;
 };
 
 export const addContact = async data => {
-  const { data: result } = await contactInstance.post('/', data);
-  // console.log('result: ', result);
+  console.log('data addContact: ', data);
+  const { data: result } = await instance.post('/contacts', data);
   return result;
 };
 
 export const deleteContacts = async id => {
-  const { data } = await contactInstance.delete(`/${id}`);
+  const { data } = await instance.delete(`/contacts/${id}`);
   return data;
 };
